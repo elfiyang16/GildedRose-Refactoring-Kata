1de8233b07e8c8ceeace021ee5e305952ea14aa0
elfi yang <elfiyang16@gmail.com>
2020-01-23 10:52:51 +0000

    test: aged brie quality increases by 1 when past sell in

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -4,7 +4,7 @@ var { Shop, Item } = require("../src/gilded_rose.js");
 
 describe("Gilded Rose", function() {
   describe("1. Normal item", function() {
-    it("a. can reduce quality and sellIn correctly when update", function() {
+    it("a. can reduce quality and sellIn correctly by 1 when update", function() {
       const cursedRelic = new Item("Cursed Relic", 9, 9);
       const gildedRose = new Shop([cursedRelic]);
       const items = gildedRose.updateQuality();
@@ -22,6 +22,15 @@ describe("Gilded Rose", function() {
       const gildedRose = new Shop([cursedRelic]);
       const items = gildedRose.updateQuality();
       expect(items[0]).toEqual(new Item("Cursed Relic", -4, 0));
+    });
+  });
+
+  describe("2. Aged Brie", function() {
+    it("a. quality increases by 1 when past sell in", function() {
+      const brie = new Item("Aged Brie", 10, 10);
+      const gildedRose = new Shop([brie]);
+      const items = gildedRose.updateQuality();
+      expect(items[0]).toEqual(new Item("Aged Brie", 9, 11));
     });
   });
 });
